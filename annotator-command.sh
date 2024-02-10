164117689deb6412562b629e996467e8b4c487b2
nimakarimipour <karimipour.nima@gmail.com>
2024-02-09 17:22:20 -0800

    update configuration

---
+++ annotator-command.sh
@@ -0,0 +1,7 @@
+lib=true
+arg=true
+poly=true
+cf_version="3.42.0"
+ucrt_version="0.1-alpha-2"
+rm -rvf target
+CF_VERSION=$cf_version UCRT_VERSION=$ucrt_version ANNOTATOR_POLY=$poly ANNOTATOR_LIBRARY=$lib ANNOTATOR_TYPE_ARG=$arg mvn compile
